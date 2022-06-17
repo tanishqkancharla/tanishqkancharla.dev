e264355b8a618722fb0706dd85255b9e42767de0
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2022-06-17 09:59:20 -0400

    start opacity 0

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const Article = styled.article`
 	display: flex;
 	flex-direction: column;
+
+	& > * {
+		// This gets animated to opacity 1 in client code
+		opacity: 0;
+	}
 `;
